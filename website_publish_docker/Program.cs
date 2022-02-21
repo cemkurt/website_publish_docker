5d0f2d488429d3c4e5d7a9e3a0e1eb0bd53aaa38
cem-kurt <cem.kurt@path.com.tr>
2022-02-21 16:24:46 +0300

    docker file

--- website_publish_docker/Program.cs
+++ website_publish_docker/Program.cs
@@ -20,6 +20,7 @@ namespace website_publish_docker
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseUrls("http://*1000");
                     webBuilder.UseStartup<Startup>();
                 });
     }
